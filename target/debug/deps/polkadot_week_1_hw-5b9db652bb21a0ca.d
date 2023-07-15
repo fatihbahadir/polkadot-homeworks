a44f55d033ad320a66203ded8da6f00997166c71
fatihbahadir <hunkarhyme@gmail.com>
2023-07-15 17:44:03 +0300

    Refresh gitignore

--- target/debug/deps/polkadot_week_1_hw-5b9db652bb21a0ca.d
+++
@@ -1,5 +0,0 @@
-/Users/fatihbahadir/Desktop/polkadot-bootcamp-homeworks/week-1/polkadot-week-1-hw/target/debug/deps/polkadot_week_1_hw-5b9db652bb21a0ca.rmeta: src/main.rs
-
-/Users/fatihbahadir/Desktop/polkadot-bootcamp-homeworks/week-1/polkadot-week-1-hw/target/debug/deps/polkadot_week_1_hw-5b9db652bb21a0ca.d: src/main.rs
-
-src/main.rs:
--- target/debug/deps/polkadot_week_1_hw-d0d199506e79fc2b.d
+++
@@ -1,5 +0,0 @@
-/Users/fatihbahadir/Desktop/polkadot-bootcamp-homeworks/week-1/polkadot-week-1-hw/target/debug/deps/polkadot_week_1_hw-d0d199506e79fc2b: src/main.rs
-
-/Users/fatihbahadir/Desktop/polkadot-bootcamp-homeworks/week-1/polkadot-week-1-hw/target/debug/deps/polkadot_week_1_hw-d0d199506e79fc2b.d: src/main.rs
-
-src/main.rs:
